bafcc164b108d3a12197cde04af4819b1eff3029
Kirandeep Kaur <200447067@student.georgianc.on.ca>
2021-04-17 20:14:25 -0400

    Added the API folder into the Controllers folder. Then added the TripsController and Values controller into the API folder

---
+++ Controllers/API/TripsController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Assignment1B.Data;
+using Assignment1B.Models;
+
+namespace Assignment1B.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TripsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Trips
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Trip>>> GetTrips()
+        {
+            return await _context.Trips.ToListAsync();
+        }
+
+        // GET: api/Trips/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Trip>> GetTrip(int id)
+        {
+            var trip = await _context.Trips.FindAsync(id);
+
+            if (trip == null)
+            {
+                return NotFound();
+            }
+
+            return trip;
+        }
+
+        // PUT: api/Trips/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTrip(int id, Trip trip)
+        {
+            if (id != trip.TripId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(trip).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TripExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Trips
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Trip>> PostTrip(Trip trip)
+        {
+            _context.Trips.Add(trip);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTrip", new { id = trip.TripId }, trip);
+        }
+
+        // DELETE: api/Trips/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Trip>> DeleteTrip(int id)
+        {
+            var trip = await _context.Trips.FindAsync(id);
+            if (trip == null)
+            {
+                return NotFound();
+            }
+
+            _context.Trips.Remove(trip);
+            await _context.SaveChangesAsync();
+
+            return trip;
+        }
+
+        private bool TripExists(int id)
+        {
+            return _context.Trips.Any(e => e.TripId == id);
+        }
+    }
+}
